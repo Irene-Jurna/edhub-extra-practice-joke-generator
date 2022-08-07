1e63ca8e817d91c9fe835b2d0415ebaa18f3724f
Irene Jurna <irene.jurna@gmail.com>
2022-08-07 18:09:01 +0200

    extra-oefenopdracht

---
+++ src/main.js
@@ -0,0 +1,24 @@
+console.log('Laten we grapjes ophalen')
+
+import giveMeAJoke from 'give-me-a-joke'
+import axios from 'axios'
+
+giveMeAJoke.getRandomDadJoke((joke) => {
+    console.log("Dad joke: " + joke);
+    const jokeTwo = document.getElementById('dad-joke');
+    jokeTwo.innerHTML = `Dad joke: ${joke}`;
+});
+
+async function fetchJoke() {
+    try {
+        const result = await axios.get('https://api.chucknorris.io/jokes/random');
+        console.log('Chuck Norris grapje: ' + result.data.value);
+        const jokeOne = document.getElementById('chuck-norris-joke');
+        jokeOne.innerHTML = `Chuck Norris joke: ${result.data.value}`;
+    } catch(e) {
+        console.error(e);
+    }
+}
+
+fetchJoke();
+
